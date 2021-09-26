2d93bfe377696b8b7f36c160b19df86d43c814f0
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-09-26 16:22:25 -0400

    commit tree 964fcf218cd581bec2cb60806b1377dd99f4913e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart ] = useState(false)
+
+  function handleInCart() {
+    setInCart((inCart) => !inCart)
+  }
+
+  const updatedCart = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={updatedCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        onClick={handleInCart}
+        className={inCart ? "remove" : "add"}>Add to Cart</button>
+        {inCart ? "Remove from" : "Add to"}
     </li>
   );
 }
